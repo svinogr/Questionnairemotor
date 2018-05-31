8be43d7a22ec8833e9bec2379de3c93781c66489
svinogr <svinogr@hotmail.com>
2018-05-31 13:53:08 +0300

    end

---
+++ app/src/main/java/info/upump/questionnairegranjpravo/db/DBDAO.java
@@ -0,0 +1,32 @@
+package info.upump.questionnairegranjpravo.db;
+
+import android.content.Context;
+import net.sqlcipher.database.SQLiteDatabase;
+
+import static info.upump.questionnairegranjpravo.db.DataBaseHelper.PASS;
+
+
+
+public class DBDAO {
+    protected SQLiteDatabase database;
+    private DataBaseHelper dataBaseHelper;
+    private Context context;
+
+    public DBDAO(Context context) {
+        this.context = context;
+        dataBaseHelper = DataBaseHelper.getHelper(context);
+        open();
+    }
+
+    public void open() {
+        if (dataBaseHelper == null)
+            dataBaseHelper = DataBaseHelper.getHelper(context);
+        database = dataBaseHelper.getWritableDatabase(PASS);
+
+    }
+
+    public void close() {
+        dataBaseHelper.close();
+        database = null;
+    }
+}
